df49f4bef4729a04fb4748cebc6ad068568cdc3c
vkrvj <31497657+vkrvj@users.noreply.github.com>
2018-02-23 13:48:45 +0530

    Create grtr.py

---
+++ beginner lvl/grtr.py
@@ -0,0 +1,12 @@
+a=int(raw_input("a="))
+print a
+b=int(raw_input("b="))
+print b
+c=int(raw_input("c="))
+print c
+if(a>b and a>c):
+	print("a is greater")
+elif(b>a and b>c):
+	print("b is greater")
+else:
+	print("c is greater")
