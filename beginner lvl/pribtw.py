af1aea86b597a9798fc7d3711a8471fb4640143d
vkrvj <31497657+vkrvj@users.noreply.github.com>
2018-03-14 16:11:42 +0530

    Create pribtw.py

---
+++ beginner lvl/pribtw.py
@@ -0,0 +1,18 @@
+def main():
+   
+
+
+ a = int(input(" "))
+ b = int(input(""))
+ for x in range(a,b + 1):
+   
+   if x > 1:
+       for i in range(2,x):
+           if (x % i) == 0:
+               break
+       else:
+           print(x)
+
+if __name__ == '__main__':
+    main()
+   
