dc3d3021ee0d0636e066bbe525e91f3446cbf634
vkrvj <31497657+vkrvj@users.noreply.github.com>
2018-03-21 15:52:50 +0530

    Create shift.py

---
+++ player/shift.py
@@ -0,0 +1,18 @@
+def rotate(a,k,l):
+	r=[]
+	for i in range(a-b,a):
+		r.append(c[i])
+	for i in range(a-b):
+		r.append(c[i])
+	print(r)
+def main():
+	try:
+		a=int(input())
+		b=int(input())
+		c=[]
+		for i in range(a):
+			l.append(int(input()))
+		rotate(a,b,c)
+	except:
+		print('invalid')
+main()
